d1c6add20069338eb829e7e3603b8ddcb9066453
adam <adam.lee346@gmail.com>
2017-08-29 20:44:45 -0700

    first commit

---
+++ HW1/UserLogin.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class UserLogin{
+  public static void main (String[] args){
+    greetUser();
+  }
+
+  public static void greetUser(){
+    System.out.println("This program validates a user name. \n" +
+                        "The following requirements must be met: \n" +
+                        "Contains at least one special Character \n" +
+                        "Contains at least one digit \n" +
+                        "Contains at least one uppercase letter \n" +
+                        "Have at least 5 characters in length.");
+  }
+
+  public static void readUser(){
+    // Read input from the User
+  }
+
+  public static void checkCase(){
+    // Check if all cases are met
+  }
+
+  public static void checkLength(){
+    // Check if the length is met
+  }
+
+  public static void checkValidty(){
+    // Check if the user name is valid
+  }
+
+  public static void printUser(){
+    // Notify user if username is valid or not
+  }
+
+  public static void addToReport(){
+    // Concatonate with reports
+  }
+
+  public static void printReport(){
+    // Write to an output file
+  }
+}
---
+++ extras/Project1.java
@@ -0,0 +1,12 @@
+import Java.utils.*;
+import Java.utils.Scanner;
+import Java.io.*;
+
+public class Project1{
+  public static void main (String[] args)
+  {
+    File input_file = new File("Test.txt");
+    File output_file = new File("OutputFile.txt");
+    
+  }
+}
