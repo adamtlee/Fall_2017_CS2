22881e169a9f398d7d4d11f76fba7b8888174b58
adam <adam.lee346@gmail.com>
2017-09-07 10:38:49 -0700

    Userlogin2 modifications in progress

--- HW1/UserLogin2.java
+++ HW1/UserLogin2.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class UserLogin2{
     public static void main(String[] args) {
@@ -12,8 +15,6 @@ public class UserLogin2{
       Scanner input = new Scanner (System.in);
 
       checkCase(username);
-      passCheck(username);
-
   }
   public static void greetUser(){
     System.out.println("This program validates a username. \n" +
@@ -61,36 +62,18 @@ public class UserLogin2{
         System.out.println("Username cannot contain a space.");
         valid = false;
     }
+    if(valid){
+        System.out.println("Username is valid.");
+    }
   }
 
-  public static void passCheck(String username){
-      boolean valid = true;
-      if(username.length() < 5){
-          System.out.println("Username is not five characters long.");
-          valid = false;
-      }
-      String upperCase = "(.*[A-Z].*)";
-      if(!username.matches(upperCase)){
-          System.out.println("Username must contain at least one capital letter.");
-          valid = false;
-      }
-      String numbers = "(.*[0-9].*)";
-      if(!username.matches(numbers)){
-          System.out.println("Username must contain at least one number.");
-          valid = false;
-      }
-      String specialChars = "(.*[ ! # @ $ % ^ & * ( ) - _ = + [ ] ; : ' \" , < . > / ?].*)";
-      if(!username.matches(specialChars)){
-          System.out.println("Username must contain at least one special character.");
-          valid = false;
-      }
-      String space = "(.*[   ].*)";
-      if(username.matches(space)){
-          System.out.println("Username cannot contain a space.");
-          valid = false;
-      }
-      if(valid){
-          System.out.println("Username is valid.");
-      }
+  public static void printUser(String LoginValidity){
+    try{
+      PrintWriter writer = new PrintWriter("Output.txt", "UTF-8");
+      writer.println("Login: " + LoginValidity);
+
+    } catch (IOException e){
+      System.out.println("Error: " + e);
+    }
   }
 }
