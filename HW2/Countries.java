4516c10c85655beb60c0be8b77183f0f4cb44cd4
adam <adam.lee346@gmail.com>
2017-09-09 15:44:57 -0700

    HW2 in progress

---
+++ HW2/Countries.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Countries {
+  String countryName;
+  String latitude;
+  String longitude;
+  int countryArea;
+  int countryPopulation;
+  double countryGDP;
+  int countryYear; 
+}
---
+++ HW2/HW2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HW2 {
+  public static void main (String[] args){
+    HW2 runnable = new HW2();
+    runnable.promptUser();
+  }
+
+  public static void promptUser(){
+    System.out.println("Promptuser method TODO");
+  }
+}
